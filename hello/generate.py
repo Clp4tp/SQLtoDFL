4837bc231fa3bd11c65c56ead09956d9232cfa5d
Clp4tp <dzampouras@gmail.com>
2016-02-27 16:53:46 +0200

    Count scripts in hello

---
+++ hello/generate.py
@@ -0,0 +1,13 @@
+import codecs
+import random, string
+def randomword(length):
+   return ''.join(random.choice(string.lowercase) for i in range(length))
+
+if __name__ == '__main__':
+	filename = raw_input("Give a filename: ")
+	f = codecs.open(filename, 'w', 'utf-8')
+	rows =  int(raw_input("Give rows: "))
+	for i in range(0,rows): 	
+		f.write("%s|%s|%s|\n" %(i, randomword(5), i+40)) 
+	print "end"
+		
